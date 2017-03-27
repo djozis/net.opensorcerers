b2ae596d4cd506eef4b237cf2842039d10218e9e
David Jozis <david.jozis@gmail.com>
2017-03-27 07:27:09 -0400

    Added disown and propagating return code

--- .travis/check.sh
+++ .travis/check.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 pwd
-./gradlew cgroup check &
+(./gradlew cgroup check; echo "$?" >> "$HOME/check-result") &
 CHECK=$!
+disown
 echo "CHECK is $CHECK"
 while kill -0 "$CHECK"; do
     sleep 1
 done
+exit $(head -n 1 "$HOME/check-result")
